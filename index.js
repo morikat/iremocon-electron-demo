cb8a73a5cd14c615fbb4bcea2da317fd14c2b770
morikat <morika-t@outlook.com>
2015-08-23 01:35:03 +0900

    Enable DevTools & shortcut key

--- index.js
+++ index.js
@@ -24,8 +24,75 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
+  // Open the devtools.
+  //mainWindow.openDevTools();
+  installMenu();
+
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
 });
+
+function installMenu() {
+  var Menu = require('menu');
+  if(process.platform == 'darwin') {
+    var menu = Menu.buildFromTemplate([
+      {
+        label: 'Electron',
+        submenu: [
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Toggle Full Screen',
+            accelerator: 'Ctrl+Command+F',
+            click: function() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+          },
+          {
+            label: 'Toggle Developer Tools',
+            accelerator: 'Alt+Command+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          },
+        ]
+      }
+    ]);
+    Menu.setApplicationMenu(menu);
+  } else {
+    var menu = Menu.buildFromTemplate([
+      {
+        label: '&View',
+        submenu: [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Toggle &Full Screen',
+            accelerator: 'F11',
+            click: function() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+          },
+          {
+            label: 'Toggle &Developer Tools',
+            accelerator: 'Alt+Ctrl+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          },
+        ]
+      }
+    ]);
+    mainWindow.setMenu(menu);
+  }
+}
